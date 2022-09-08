703e4d6f9f51250fbe08df1de5e6786fb63809ab
Vincent_Reix_54 <vincentreix54@gmail.com>
2022-09-08 23:58:55 +0200

    Adding English Text And Some NPM install

--- Code/index.js
+++ Code/index.js
@@ -1,14 +1,18 @@
-// Ajout des de la classe discord.js dans mon répertoire + de mon token dans mon fichier config.json
+// FR : Ajout de la classe discord.js dans mon répertoire + de mon token dans mon fichier config.json
+// EN : Require the necessary discord.js classes
 const { Client, GatewayIntentBits } = require('../Discord/node_modules/discord.js');
 const { token } = require("../Config/config.json");
 
-// Créer une nouvelle instance de client
+// FR : Créer une nouvelle instance de client
+// EN : Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Une fois que le client est prêt, le code ci-dessous va être exécuté (une seule fois)
+// FR : Une fois que le client est prêt, le code ci-dessous va être exécuté (une seule fois)
+// EN : When the client is ready, run this code (only once)
 client.once('ready',() => {
     console.log("Je suis prêt !");
 });
 
-// Connexion à Discord grâce au token de notre client
+// FR : Connexion à Discord grâce au token de notre client
+// EN : Login to Discord with your client's token
 client.login(token);
