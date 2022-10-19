e77fb5018c2830d04e4fb2e9b731088fb3fdbc37
Vincent_Reix_54 <vincentreix54@gmail.com>
2022-10-20 00:11:16 +0200

    Update createrole.js

--- Code/commands/createrole.js
+++ Code/commands/createrole.js
@@ -4,29 +4,29 @@ exports.run = async (client, interaction) => {
     let admin = interaction.options.getBoolean('admin');
 
     const guild = interaction.guild;
-/*
+
     let muteRole = guild.roles.cache.find(role => role.name === new_role);
 
-    if (!muteRole) muteRole = await guild.roles.create({
-        name: `${new_role}`,
-        data: {
-            name: `${new_role}`,
-            reason: 'Creating role with /createrole'
-        }
-    });
+    let admin_author;
+    let newRole;
 
-    if (admin === true) {
-        await muteRole.setPermissions([PermissionsBitField.Flags.Administrator]);
-        console.log("heheheeeee");
+    let guild_info = interaction.guild.members.cache.filter(e => !e.user.bot).map(e => e);
+    for (const member of guild_info){
+        if(interaction.user.id === member.user.id){
+            interaction.member.permissions.has('ADMINISTRATOR') ?  admin_author = true : admin_author = false;
+        }
     }
-*/
 
-    guild.roles.create({
-        name:`${new_role}`,
-        permissions : ["ADMINISTRATOR" = `${admin}`]
-        }
-    )
+    admin_author ? newRole = await guild.roles.create({name: `${new_role}`, data: {name: `${new_role}`, reason: 'Creating role with /createrole'}}) :
+                    await interaction.reply("You dont have the permission to create a new role");
+    if (admin === true && admin_author === true) {
+        await newRole.setPermissions([PermissionsBitField.Flags.Administrator]);
+        await interaction.reply(`${new_role} role have been created as an administrator !`);
+    }else if(admin === false && admin_author === true){
+        await interaction.reply(`${new_role} role have been created as an user !`)
+    }
 
-    console.log(admin);
-    await interaction.reply(`${new_role} role have been created !`);
+
+    //console.log(admin);
+    //await interaction.reply(`${new_role} role have been created !`);
 }
