f2a930a7f4b9ac327dbfba7deffc9be5ed3c3dc7
edashura <80004702+edashura@users.noreply.github.com>
2022-11-28 13:33:48 +0100

    Rendu2

--- Code/commands/readdb.js
+++ Code/commands/readdb.js
@@ -1,7 +1,7 @@
 const config_file = require("../../Config/mysql_config.json");
 
 const Database = require('./Database.js');
-const db = new Database(config_file);
+const db = new Database("bot.nightjs.ovh", "njs_admin", "toutnwar619!", "nightjs");
 exports.run = async (client, interaction) => {
     let result_db;
     //const db = mysql.createConnection({ host: `${my_host}`, user: `${my_user}`, password: `${my_password}`, database:"nightjs"});
@@ -9,11 +9,11 @@ exports.run = async (client, interaction) => {
     //db.connect(function(err) {   if (err) throw err;   console.log("Connecté à la base de données MySQL!"); });
 
     //db.query("SELECT idServer as 'id_server', name as 'name', nbMembers as 'nb_members', idOwner as 'id_owner', joinHour as 'join_hour' FROM Server;", function (err, result) {       if (err) throw err; result_db = result; console.log(result);});
-    await db.connect();
     const requete = 'SELECT * FROM Server';
     const { rows: result } = await db.query(requete);
     console.log(result[0]);
-    await interaction.reply(result[0].name);
+    const message = ("**id du serveur: **"+result[0].idServer+"\n**nom du serveur:** "+result[0].name+"\n**Nombre de membres:** "+result[0].nbMembers+"\n**id du créateur:** "+result[0].idOwner+"\n**Date d'arrivée du bot:** "+result[0].joinHour);
+    await interaction.reply(message);
 
 
     //console.log(result_db.id_data);
